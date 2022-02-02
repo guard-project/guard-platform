228ccde9d98c273f6e90f89693d85580cb7002c5
Alex Carrega <contact@alexcarrega.com>
2022-02-02 16:34:48 +0100

    Minor fix

--- scripts/clean-gh-action.sh
+++ scripts/clean-gh-action.sh
@@ -2,6 +2,7 @@
 
 user=guard-project
 repo=guard-platform
+
 gh api repos/$user/$repo/actions/runs \
     --paginate -q '.workflow_runs[] | "\(.id)"' | \
     xargs -I % gh api repos/$user/$repo/actions/runs/% -X DELETE
